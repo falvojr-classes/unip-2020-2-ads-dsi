40f01192e390b2860c1208f81406a7fc654ab200
Venilton FalvoJr <falvojr@gmail.com>
2020-10-14 22:01:30 -0300

    Aula 14/10

--- 6. PIM/home/home-adm.js
+++ 6. PIM/home/home-adm.js
@@ -16,30 +16,59 @@ async function buscarUsuarios() {
                 gridUsuarios.innerHTML = '';
 
                 const usuarios = await response.json();
-                usuarios.forEach(usuario => {
-                    const id = criarElementoComClasse('div', 'col-1', usuario.id);
-                    const nome = criarElementoComClasse('div', 'col-3', usuario.nome);
-                    const documento = criarElementoComClasse('div', 'col-2', usuario.documento);
-                    const email = criarElementoComClasse('div', 'col-3', usuario.email);
-                    const tipo = criarElementoComClasse('div', 'col-1', usuario.tipo);
 
-                    const acoes = criarElementoComClasse('div', 'col-2');
-                    const iconeExcluir = criarElemento('img');
-                    iconeExcluir.src = '../base/icons/delete.svg';
-                    iconeExcluir.onclick = function(){
-                        const confirmou = confirm(`Deseja excluir o usuário de ID ${usuario.id}?`);
-                        if (confirmou) {
-                            excluirUsuario(usuario.id);
+                for (let i = 0; i < usuarios.length; i++) {
+                    const usuario = usuarios[i];
+                    const id = criarElementoComClasse('div', usuario.id, 'col-1', 'center');
+                    const nome = criarElementoComClasse('div', usuario.nome, 'col-4', 'center');
+                    const email = criarElementoComClasse('div', usuario.email, 'col-4', 'center');
+                    const tipo = criarElementoComClasse('div', usuario.tipo, 'col-1', 'center');
+
+                    const acoes = criarElementoComClasse('div', '', 'col-2', 'center');
+                    const iconeVisualizar = criarElemento('img');
+                    iconeVisualizar.classList.add('icon');
+                    iconeVisualizar.src = '../base/icons/view.svg';
+                    iconeVisualizar.onclick = function(){
+
+                        const dialog = getById('usuarioDialog');
+                        getById('dialogTipo').innerHTML = usuario.tipo;
+                        getById('dialogNome').innerHTML = usuario.nome;
+                        getById('dialogEmail').innerHTML = usuario.email;
+                        getById('dialogDocumento').innerHTML = usuario.documento;
+                        getById('dialogInteresses').innerHTML = usuario.interesses.map(e => e.descricao).join(", ");
+                        
+                        const btnDesBloquear = getById('dialogBtnDesBloquear');
+                        const acao = usuario.bloqueado ? 'desbloquear' : 'bloquear';
+                        btnDesBloquear.innerHTML = acao;
+                        if (usuario.bloqueado) {
+                            btnDesBloquear.classList.remove('warning');
+                        } else {
+                            btnDesBloquear.classList.add('warning');
                         }
-                    };
-                    acoes.appendChild(iconeExcluir);
+                        btnDesBloquear.onclick = function(){
+                            const confirmou = confirm(`Deseja ${acao} o usuário ${usuario.nome}?`);
+                            if (confirmou) {
+                                bloquearUsuario(usuario);
+                            }
+                            dialog.close();
+                        };
 
-                    const linha = criarElementoComClasse('div', 'row');
-                    [id, nome, documento, email, tipo, acoes].forEach(coluna => {
+                        getById('dialogBtnFechar').onclick = function(){
+                            dialog.close();
+                        }
+
+                        dialog.showModal();
+                    };
+                    acoes.appendChild(iconeVisualizar);
+
+                    const estiloLinha = i%2 == 0 ? 'row-pair' : 'row-odd';
+                    const estiloBloqueado = usuario.bloqueado ? 'row-blocked' : '';
+                    const linha = criarElementoComClasse('div', '', 'row', estiloLinha, estiloBloqueado);
+                    [id, nome, email, tipo, acoes].forEach(coluna => {
                         linha.appendChild(coluna);
                     });
                     gridUsuarios.appendChild(linha);
-                });
+                }
             } else {
                 const erro = await response.json();
                 alert(erro.mensagem);
@@ -54,15 +83,15 @@ async function buscarUsuarios() {
     }
 }
 
-async function excluirUsuario(id) {
+async function bloquearUsuario(usuario) {
     try {
-        const response = await fetch(`http://127.0.0.1:8080/api/usuarios/${id}`, {
+        const response = await fetch(`http://127.0.0.1:8080/api/usuarios/${usuario.id}`, {
             method: 'DELETE',
             headers: { 'Authorization': buscarLocalmente(KEY_TOKEN) }
         });
 
         if (response.ok) {
-            alert('Usuario excluído com sucesso!');
+            alert(`Usuario ${usuario.bloqueado ? 'des' : ''}bloqueado com sucesso!`);
             buscarUsuarios();
         } else {
             const erro = await response.json();
@@ -75,11 +104,15 @@ async function excluirUsuario(id) {
         }
 }
 
-function criarElementoComClasse(tag, cssClass, text) {
+function criarElementoComClasse(tag, text, ...cssClasses) {
     const elemento = criarElemento(tag);
-    elemento.classList.add(cssClass);
     if (text) {
         elemento.innerHTML = text;
     }
+    cssClasses.forEach(cssClass => {
+        if (cssClass) {
+            elemento.classList.add(cssClass);
+        }
+    })
     return elemento;
 }
